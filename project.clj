084059147e39d2c271bcea624b77e584a4d621a8
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-12-11 17:37:44 +0100

    project: release 0.1.3

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject spootnik/raven "0.1.3-SNAPSHOT"
+(defproject spootnik/raven "0.1.3"
   :description "clojure sentry client library"
   :url "https://github.com/pyr/raven"
   :license {:name "MIT License"}
   :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [spootnik/net        "0.3.3-beta9"]
-                 [cheshire            "5.7.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [spootnik/net        "0.3.3-beta13"]
+                 [cheshire            "5.8.0"]])
