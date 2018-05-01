da3c0ee20768331907fa12b2e8cbd71bccac100f
Chris Glass <chris@exoscale.ch>
2018-05-01 16:25:21 +0200

    Added frist tests.
    They don't bring much to the table but should at least excercise some of
    the pure parts.

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
   :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [spootnik/net        "0.3.3-beta24"]
-                 [cheshire            "5.8.0"]])
+                 [cheshire            "5.8.0"]]
+  :plugins [[lein-cljfmt "0.5.7"]]                 
+)
---
+++ test/raven/client_test.clj
@@ -0,0 +1,26 @@
+(ns raven.client-test
+    (:require [clojure.test :refer :all]
+              [raven.client :refer :all]))
+
+(def dsn-fixture
+    "https://098f6bcd4621d373cade4e832627b4f6:ad0234829205b9033196ba818f7a872b@sentry.example.com/42")
+
+(def expected-parsed-dsn
+    {:key "098f6bcd4621d373cade4e832627b4f6"
+     :secret "ad0234829205b9033196ba818f7a872b"
+     :uri "https://sentry.example.com"
+     :pid 42}
+)
+
+(def frozen-ts
+    "2018-05-01 16:20:44.955")
+
+(deftest raven-client-tests
+    (testing "parsing DSN"
+       (is (= (parse-dsn dsn-fixture) expected-parsed-dsn)) 
+    )
+
+    (testing "signing"
+        (is (= (sign "payload" frozen-ts (:key expected-parsed-dsn) (:secret expected-parsed-dsn)) "da3edd4ce3c55f1552fb01108f974a1181513323"))
+    )
+)
