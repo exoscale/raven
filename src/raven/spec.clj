5a79d80b70d0419c87c48027c0172da3c1642960
Chris Glass <chris@exoscale.ch>
2018-05-03 10:42:37 +0200

    Fixed semicolon comments.

--- src/raven/spec.clj
+++ src/raven/spec.clj
@@ -4,6 +4,6 @@
   (:require [clojure.spec.alpha :as s]
             [expound.alpha      :as expound]))
 
-; We declare the message spec in the raven.client namespace to allow easy
-; reference from there (simply "::payload" when using the spec).
+;; We declare the message spec in the raven.client namespace to allow easy
+;; reference from there (simply "::payload" when using the spec).
 (s/def :raven.client/payload (s/keys :req-un [::level ::server_name ::timestamp ::platform]))
