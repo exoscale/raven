7c38254e4d1354973c6e323db1721b2a1277a686
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-05-31 15:33:57 +0200

    release 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/raven "0.3.1"
+(defproject exoscale/raven "0.3.2"
   :description "clojure sentry client library"
   :url "https://github.com/exoscale/raven"
   :license {:name "MIT License"}
