3604e36d0be9f02b7764cdbf6fe67e8d06097060
Chris Glass <chris@exoscale.ch>
2018-07-11 09:39:00 +0200

    Real 0.4.3 release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/raven "0.4.3-SNAPSHOT"
+(defproject exoscale/raven "0.4.3"
   :description "clojure sentry client library"
   :url "https://github.com/exoscale/raven"
   :license {:name "MIT License"}
