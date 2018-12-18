7896f4644330855416b10e1e5be97703658a5283
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-12-18 13:13:03 +0100

    deps: bump depenencies
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject exoscale/raven "0.4.6"
+(defproject exoscale/raven "0.4.7-SNAPSHOT"
   :description "clojure sentry client library"
   :url "https://github.com/exoscale/raven"
   :license {:name "MIT License"}
   :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [aleph               "0.4.6"]
                  [metosin/jsonista    "0.2.2"]
-                 [org.flatland/useful "0.11.5"]]
+                 [org.flatland/useful "0.11.6"]]
   :test-selectors {:default (complement :integration-test)
                    :integration :integration-test})
