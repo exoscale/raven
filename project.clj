c5d31d1697263440a24782206a7c3732a3dfc484
Pierre-Yves Ritschard <pyr@spootnik.org>
2020-03-17 11:40:57 +0100

    project: support lein release

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
                  [aleph               "0.4.6"]
                  [metosin/jsonista    "0.2.2"]
                  [org.flatland/useful "0.11.6"]]
+  :deploy-repositories [["snapshots" :clojars] ["releases" :clojars]]
   :test-selectors {:default (complement :integration-test)
                    :integration :integration-test})
