0dbf4790909d7beeb94952a4a39608be126b504d
Ivan Grishaev <ivan@grishaev.me>
2020-04-06 12:56:19 +0300

    Version 0.4.13-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/raven "0.4.12"
+(defproject exoscale/raven "0.4.13-SNAPSHOT"
   :description "clojure sentry client library"
   :url "https://github.com/exoscale/raven"
   :license {:name "MIT License"}
